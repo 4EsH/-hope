97627e31d78e7154cda173e97e2c07f8077431cc
Daniel Walerzak <danexhd@gmail.com>
2018-09-09 17:54:25 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello
 } 
 	public static void main (String[] args)
 	{
-		System.out.println("Hello World") ;
-		System.out.println("Just believe, everything is possible") ;
+		System.out.println("Hello World");
+		System.out.println("Just believe, everything is possible");
 	}
 }
 ...
