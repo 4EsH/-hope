92fc5a4faeaf96cbeeaf74f6d95a622708d11545
Daniel Walerzak <danexhd@gmail.com>
2018-09-09 18:21:37 +0200

    Create Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        byte zero = 0;
+        int a = 1;
+        char c = ' ';
+        short d = 3;
+        float e = 2.0f;
+        boolean f = true;
+        String output = "H" + d + a + a + zero + c + "w" + zero + "r" + a + "d" + c + e + c + f;
+        System.out.println(output);
+    }
+}
